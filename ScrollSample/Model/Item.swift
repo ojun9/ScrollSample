8f9d1df61a8dd89e9208ad711db6ca08d97948d6
ojun9 <eu.bkw7jdcpa2r@gmail.com>
2023-11-27 23:59:56 +0900

    あっd

---
+++ ScrollSample/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  ScrollSample
+//
+//  Created by jun on 2023/11/27.
+//
+
+import Foundation
---
+++ ScrollSample/Model/View/LoopingScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  LoopingScrollView.swift
+//  ScrollSample
+//
+//  Created by jun on 2023/11/27.
+//
+
+import SwiftUI
+
+struct LoopingScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoopingScrollView()
+}
