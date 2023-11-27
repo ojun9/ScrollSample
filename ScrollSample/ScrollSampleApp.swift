e237629953c66cc0484681f65afbc7fe5bb031ab
ojun9 <eu.bkw7jdcpa2r@gmail.com>
2023-11-27 22:59:43 +0900

    Initial Commit

---
+++ ScrollSample/ScrollSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollSampleApp.swift
+//  ScrollSample
+//
+//  Created by jun on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct ScrollSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
